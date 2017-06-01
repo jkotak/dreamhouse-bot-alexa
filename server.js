32c9ef8f44c644fa5ccf66bb3000f332a65f8121
jkotak <kotakj@gmail.com>
2017-06-01 16:24:50 -0700

    Update server.js

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ app.post('/dreamhouse',requestVerifier, (req, res) => {
             let handler = handlers[intent];
             if (handler) {
                 if(handler==='ContactLoanOfficer'){
-                    handler(dialogState,slots, session, response);
+                    handler(intent, dialogState,slots, session, response);
                 }else{
                     handler(slots, session, response);
                 }
